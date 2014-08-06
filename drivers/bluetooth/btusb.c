21eb437f7e475baae771b3444d1279518a024d36
Koen Kooi <koen@dominion.thruhere.net>
2014-08-06 14:08:07 +0200

    btusb: add USB ID for ASUS X79-DELUXE onboard BT
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -105,6 +105,7 @@ static const struct usb_device_id btusb_table[] = {
 	{ USB_DEVICE(0x04ca, 0x2003) },
 	{ USB_DEVICE(0x0b05, 0x17b5) },
 	{ USB_DEVICE(0x0b05, 0x17cb) },
+	{ USB_DEVICE(0x0b05, 0x17cf) },
 	{ USB_DEVICE(0x413c, 0x8197) },
 
 	/* Foxconn - Hon Hai */
